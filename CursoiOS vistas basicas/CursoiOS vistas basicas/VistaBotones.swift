e871024a68c4f0b6b409a066ca1458ec62c521a5
cursoiosxcode2026 <pvaquerotirado@gmail.com>
2026-01-20 14:43:00 +0100

    20 enero

---
+++ CursoiOS vistas basicas/CursoiOS vistas basicas/VistaBotones.swift
@@ -0,0 +1,18 @@
+//
+//  VistaBotones.swift
+//  CursoiOS vistas basicas
+//
+//  Created by Equipo 7 on 20/1/26.
+//
+
+import SwiftUI
+
+struct VistaBotones: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VistaBotones()
+}
