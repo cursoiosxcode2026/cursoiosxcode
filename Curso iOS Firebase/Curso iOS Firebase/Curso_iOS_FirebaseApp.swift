d0f0dcea78d2900771b5a19cb63ccbb71532a7d7
Paloma <pvaquerotirado@gmail.com>
2026-03-02 14:55:04 +0100

    2 de marzo

--- Curso iOS Firebase/Curso iOS Firebase/Curso_iOS_FirebaseApp.swift
+++ Curso iOS Firebase/Curso iOS Firebase/Curso_iOS_FirebaseApp.swift
@@ -6,9 +6,22 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+      
+      print("🔥 Firebase configurado correctamente!")
+    return true
+  }
+}
 
 @main
 struct Curso_iOS_FirebaseApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ Curso iOS Firebase/Curso iOS Firebase/Vistas/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Curso iOS Firebase
+//
+//  Created by Equipo 7 on 2/3/26.
+//
+
+import SwiftUI
+import FirebaseAuth
+
+struct ContentView: View {
+    
+    @State private var manager = AuthManager()
+    
+    var body: some View {
+
+        Group {
+            if let user = manager.user {
+                VistaGastos(idUsuario: user.uid)
+            } else {
+                VistaLogin(authManager: manager)
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
