7b92f611680b6d476bb7deaf91e5817aea73ed89
Paloma <pvaquerotirado@gmail.com>
2026-03-06 14:35:26 +0100

    6 de marzo

--- RedSocialMenu/RedSocialMenu/Vistas/VistaFeed.swift
+++ RedSocialMenu/RedSocialMenu/Vistas/VistaFeed.swift
@@ -48,6 +48,7 @@ struct VistaFeed: View {
                     }
                 }
             }
+            .padding()
             .task {
                 await viewModel.cargarDatosCompletos()
             }
